aa0d7b62cab8da82ca097ff3ad59b462535e3394
Tyler Lampel <lampel13@gmail.com>
2022-10-29 20:34:13 -0400

    added controllers for items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,28 @@ class ApplicationController < Sinatra::Base
     ["Good luck with your project!"].to_json
   end
 
+  post "/items" do
+    item = Item.create(task: params[:task],
+      importance: params[:importance],
+      list_id: params[:list_id])
+    item.to_json(include: { list: {only: [:list]} })
+  end
+
+  get "/items" do
+    items = Item.all.order(:list_id)
+    items.to_json(include: {list: {only: [:list]} })
+  end
+
+  patch "/items/:id" do
+    item = Item.find(params[:id])
+    item.update(task: params[:task])
+    item.to_json(include: { list: {only: [:list]} })
+  end
+
+  delete "/items/:id" do
+    item = Item.find(params[:id])
+    item.destroy
+    item.to_json
+  end
 
 end
